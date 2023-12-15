7c63581e656b6b25280f01f979fc069f7b30bd07
Himanshu Bansal <bansah@usi.ch>
2023-12-15 17:00:33 +0100

    added 3rd dimension

--- ThermalConvection3D.jl
+++ ThermalConvection3D.jl
@@ -10,6 +10,14 @@ using ImplicitGlobalGrid, Plots, Printf, Statistics, LinearAlgebra
 import MPI
 #Global reductions
 maximum_g(A) = (max_l  = maximum(A); MPI.Allreduce(max_l,  MPI.MAX, MPI.COMM_WORLD))
+@views inn(A)   =  A[2:end-1,2:end-1,2:end-1]
+
+function save_array(Aname, A)
+    fname = string(Aname, ".bin")
+    out = open(fname, "w")
+    write(out, A)
+    close(out)
+end
 
 @parallel function assign!(A::Data.Array, B::Data.Array)
     @all(A) = @all(B)
@@ -41,7 +49,7 @@ end
 
 @parallel function compute_2!(Rx::Data.Array, Ry::Data.Array, Rz::Data.Array, dVxdτ::Data.Array, dVydτ::Data.Array, dVzdτ::Data.Array, Pt::Data.Array, RogT::Data.Array, τxx::Data.Array, τyy::Data.Array, τzz::Data.Array, σxy::Data.Array, σxz::Data.Array, σyz::Data.Array, ρ::Data.Number, dampX::Data.Number, dampY::Data.Number, dampZ::Data.Number, dτ_iter::Data.Number, dx::Data.Number, dy::Data.Number, dz::Data.Number)
     @all(Rx)    = 1.0/ρ *(@d_xa(τxx)/dx + @d_ya(σxy)/dy + @d_za(σxz)/dz - @d_xi(Pt)/dx)
-    @all(Ry)    = 1.0/ρ *(@d_ya(τyy)/dy + @d_xa(σxy)/dx + @d_za(σxz)/dz - @d_yi(Pt)/dy)
+    @all(Ry)    = 1.0/ρ *(@d_ya(τyy)/dy + @d_xa(σxy)/dx + @d_za(σyz)/dz - @d_yi(Pt)/dy)
     @all(Rz)    = 1.0/ρ *(@d_za(τzz)/dz + @d_xa(σxz)/dx + @d_ya(σyz)/dy - @d_zi(Pt)/dz + @av_zi(RogT))
     @all(dVxdτ) = dampX*@all(dVxdτ) + @all(Rx)*dτ_iter
     @all(dVydτ) = dampY*@all(dVydτ) + @all(Ry)*dτ_iter
@@ -63,12 +71,14 @@ end
     return
 end
 
-@parallel_indices (ix,iy) function advect_T!(dT_dt::Data.Array, qTx::Data.Array, qTy::Data.Array, T::Data.Array, Vx::Data.Array, Vy::Data.Array, dx::Data.Number, dy::Data.Number)
-    if (ix<=size(dT_dt, 1) && iy<=size(dT_dt, 2)) dT_dt[ix,iy] = -((qTx[ix+1,iy]-qTx[ix,iy])/dx + (qTy[ix,iy+1]-qTy[ix,iy])/dy) -
-                                                                  (Vx[ix+1,iy+1]>0)*Vx[ix+1,iy+1]*(T[ix+1,iy+1]-T[ix  ,iy+1])/dx -
-                                                                  (Vx[ix+2,iy+1]<0)*Vx[ix+2,iy+1]*(T[ix+2,iy+1]-T[ix+1,iy+1])/dx -
-                                                                  (Vy[ix+1,iy+1]>0)*Vy[ix+1,iy+1]*(T[ix+1,iy+1]-T[ix+1,iy  ])/dy -
-                                                                  (Vy[ix+1,iy+2]<0)*Vy[ix+1,iy+2]*(T[ix+1,iy+2]-T[ix+1,iy+1])/dy   end
+@parallel_indices (ix,iy,iz) function advect_T!(dT_dt::Data.Array, qTx::Data.Array, qTy::Data.Array, qTz::Data.Array, T::Data.Array, Vx::Data.Array, Vy::Data.Array, Vz::Data.Array, dx::Data.Number, dy::Data.Number, dz::Data.Number)
+    if (ix<=size(dT_dt, 1) && iy<=size(dT_dt, 2) && iz<=size(dT_dt, 3)) dT_dt[ix,iy,iz] = -((qTx[ix+1,iy,iz]-qTx[ix,iy,iz])/dx + (qTy[ix,iy+1,iz]-qTy[ix,iy,iz])/dy + (qTz[ix,iy,iz+1]-qTz[ix,iy,iz])/dz) -
+                                                                  (Vx[ix+1,iy+1,iz+1]>0)*Vx[ix+1,iy+1,iz+1]*(T[ix+1,iy+1,iz+1]-T[ix  ,iy+1,iz+1])/dx -
+                                                                  (Vx[ix+2,iy+1,iz+1]<0)*Vx[ix+2,iy+1,iz+1]*(T[ix+2,iy+1,iz+1]-T[ix+1,iy+1,iz+1])/dx -
+                                                                  (Vy[ix+1,iy+1,iz+1]>0)*Vy[ix+1,iy+1,iz+1]*(T[ix+1,iy+1,iz+1]-T[ix+1,iy  ,iz+1])/dy -
+                                                                  (Vy[ix+1,iy+2,iz+1]<0)*Vy[ix+1,iy+2,iz+1]*(T[ix+1,iy+2,iz+1]-T[ix+1,iy+1,iz+1])/dy -
+                                                                  (Vz[ix+1,iy+1,iz+1]>0)*Vy[ix+1,iy+1,iz+1]*(T[ix+1,iy+1,iz+1]-T[ix+1,iy+1,iz  ])/dz -
+                                                                  (Vz[ix+1,iy+1,iz+2]<0)*Vy[ix+1,iy+1,iz+2]*(T[ix+1,iy+1,iz+2]-T[ix+1,iy+1,iz+1])/dz  end
     return
 end
 
@@ -107,7 +117,7 @@ end
 end
 
 ##################################################
-@views function ThermalConvection2D()
+@views function ThermalConvection3D()
     # Physics - dimentionally independent scales
     ly        = 1.0                # domain extend, m
     η0        = 1.0                # viscosity, Pa*s
@@ -124,9 +134,9 @@ end
     ρ0gα      = Ra*η0*DcT/ΔT/ly^3  # thermal expansion
     dη_dT     = 1e-10/ΔT           # viscosity's temperature dependence
     # Numerics
-    nx, ny, nz = 96*ar-1, 96-1, 96*ar-1      # numerical grid resolutions; should be a mulitple of 32-1 for optimal GPU perf
+    nx, ny, nz = 32*ar-1, 32-1, 32*ar-1      # numerical grid resolutions; should be a mulitple of 32-1 for optimal GPU perf
     iterMax   = 5*10^4             # maximal number of pseudo-transient iterations
-    nt        = 3000               # total number of timesteps
+    nt        = 30               # total number of timesteps
     nout      = 10                 # frequency of plotting
     nerr      = 100                # frequency of error checking
     ε         = 1e-4               # nonlinear absolute tolerence
@@ -146,8 +156,8 @@ end
     # Array allocations
     T         = @zeros(nx , ny, nz  )
     T        .= Data.Array([ΔT*exp(-((x_g(ix,dx,T)-0.5*lx)/w)^2 -((y_g(iy,dy,T)-0.5*ly)/w)^2 -((z_g(iz,dz,T)-0.5*lz)/w)^2) for ix=1:size(T,1), iy=1:size(T,2), iz=1:size(T,3)])
-    T[:,1  ] .=  ΔT/2.0
-    T[:,end] .= -ΔT/2.0
+    T[:,:,1] .=  ΔT/2.0
+    T[:,:,end] .= -ΔT/2.0
     T_old     = @zeros(nx  ,ny  ,nz)
     Pt        = @zeros(nx  ,ny  ,nz)
     ∇V        = @zeros(nx  ,ny  ,nz)
@@ -178,11 +188,20 @@ end
     ErrP      = @zeros(nx  ,ny  ,nz)
     ErrV      = @zeros(nx  ,ny+1)
     # Preparation of visualisation
-    ENV["GKSwstype"]="nul"; if isdir("viz2D_out")==false mkdir("viz2D_out") end; loadpath = "./viz2D_out/"; anim = Animation(loadpath,String[])
-    println("Animation directory: $(anim.dir)")
+    ENV["GKSwstype"]="nul"
+    if (me==0)
+        if isdir("viz3D_out")==false mkdir("viz3D_out") end; loadpath = "./viz3D_out/"; anim = Animation(loadpath,String[])
+        println("Animation directory: $(anim.dir)")
+    end
+    nx_v, ny_v, nz_v = (nx-2)*dims[1], (ny-2)*dims[2], (nz-2)*dims[3]
+    if (nx_v*ny_v*nz_v*sizeof(Data.Number) > 0.8*Sys.free_memory()) error("Not enough memory for visualization.") end
+    T_v   = zeros(nx_v, ny_v, nz_v) # global array for visu
+    T_inn = zeros(nx-2, ny-2, nz-2) # no halo local array for visu
+    y_sl  = Int(ceil(ny/2))
     X, Y, Z   = -lx/2:dx:lx/2, -ly/2:dy:ly/2, -lz/2:dz:lz/2
     Xc, Yc, Zc = [x for x=X, y=Y, z=Z], [y for x=X, y=Y, z=Z], [z for x=X, y=Y, z=Z]
-    Xp, Yp, Zp = Xc[1:st:end,1:st:end], Yc[1:st:end,1:st:end], Zc[1:st:end,1:st:end]
+    Xp, Yp, Zp = Xc[1:st:end,1:st:end,1:st:end], Yc[1:st:end,1:st:end,1:st:end], Zc[1:st:end,1:st:end,1:st:end]
+    Xi_g, Zi_g  = -lx/2+dx:dx:(lx/2-dx), -lz/2+dz:dz:(lz/2-dz) # inner points only
     # Time loop
     err_evo1=[]; err_evo2=[]
     for it = 1:nt
@@ -191,42 +210,54 @@ end
         while (errV > ε || errP > ε) && iter <= iterMax
             @parallel assign!(ErrV, Vy)
             @parallel assign!(ErrP, Pt)
-            @parallel compute_0!(RogT, Eta, ∇V, T, Vx, Vy, ρ0gα, η0, dη_dT, ΔT, dx, dy)
-            @parallel compute_1!(Pt, τxx, τyy, σxy, Eta, ∇V, Vx, Vy, dτ_iter, β, dx, dy)
-            @parallel compute_2!(Rx, Ry, dVxdτ, dVydτ, Pt, RogT, τxx, τyy, σxy, ρ, dampX, dampY, dτ_iter, dx, dy)
-            @parallel update_V!(Vx, Vy, dVxdτ, dVydτ, dτ_iter)
-            @parallel (1:size(Vx,1)) bc_y!(Vx)
-            @parallel (1:size(Vy,2)) bc_x!(Vy)
+            @parallel compute_0!(RogT, Eta, ∇V, T, Vx, Vy, Vz, ρ0gα, η0, dη_dT, ΔT, dx, dy, dz)
+            @parallel compute_1!(Pt, τxx, τyy, τzz, σxy, σxz, σyz, Eta, ∇V, Vx, Vy, Vz, dτ_iter, β, dx, dy, dz)
+            @parallel compute_2!(Rx, Ry, Rz, dVxdτ, dVydτ, dVzdτ, Pt, RogT, τxx, τyy, τzz, σxy, σxz, σyz, ρ, dampX, dampY, dampZ, dτ_iter, dx, dy, dz)
+            @hide_communication (16, 8, 2) begin # communication/computation overlap
+                @parallel update_V!(Vx, Vy, Vz, dVxdτ, dVydτ, dVzdτ, dτ_iter)
+                @parallel (1:size(Vy,2), 1:size(Vy,3)) bc_x!(Vy)
+                @parallel (1:size(Vz,2), 1:size(Vz,3)) bc_x!(Vz)
+                @parallel (1:size(Vx,1), 1:size(Vx,3)) bc_y!(Vx)
+                @parallel (1:size(Vz,1), 1:size(Vz,3)) bc_y!(Vz)
+                @parallel (1:size(Vx,1), 1:size(Vx,2)) bc_z!(Vx)
+                @parallel (1:size(Vy,1), 1:size(Vy,2)) bc_z!(Vy)
+                update_halo!(Vx, Vy, Vz)
+            end
             @parallel compute_error!(ErrV, Vy)
             @parallel compute_error!(ErrP, Pt)
             if mod(iter,nerr) == 0
-                errV = maximum(abs.(Array(ErrV)))/(1e-12 + maximum(abs.(Array(Vy))))
-                errP = maximum(abs.(Array(ErrP)))/(1e-12 + maximum(abs.(Array(Pt))))
+                errV = maximum_g(abs.(Array(ErrV)))/(1e-12 + maximum_g(abs.(Array(Vy))))
+                errP = maximum_g(abs.(Array(ErrP)))/(1e-12 + maximum_g(abs.(Array(Pt))))
                 push!(err_evo1, max(errV, errP)); push!(err_evo2,iter)
-                # @printf("Total steps = %d, errV=%1.3e, errP=%1.3e \n", iter, errV, errP)
+                if (me==0) @printf("Total steps = %d, errV=%1.3e, errP=%1.3e \n", iter, errV, errP) end
             end
             iter+=1; niter+=1
         end
         # Thermal solver
-        @parallel compute_qT!(qTx, qTy, T, DcT, dx, dy)
-        @parallel advect_T!(dT_dt, qTx, qTy, T, Vx, Vy, dx, dy)
-        dt_adv = min(dx/maximum(abs.(Array(Vx))), dy/maximum(abs.(Array(Vy))))/2.1
+        @parallel compute_qT!(qTx, qTy, qTz, T, DcT, dx, dy, dz)
+        @parallel advect_T!(dT_dt, qTx, qTy, qTz, T, Vx, Vy, Vz, dx, dy, dz)
+        dt_adv = min(dx/maximum_g(abs.(Array(Vx))), dy/maximum_g(abs.(Array(Vy))), dz/maximum_g(abs.(Array(Vz))))/2.1
         dt     = min(dt_diff, dt_adv)
         @parallel update_T!(T, T_old, dT_dt, dt)
         @parallel no_fluxY_T!(T)
-        @printf("it = %d (iter = %d), errV=%1.3e, errP=%1.3e \n", it, niter, errV, errP)
+        if (me==0) @printf("it = %d (iter = %d), errV=%1.3e, errP=%1.3e \n", it, niter, errV, errP) end
         # Visualization
-        if mod(it,nout)==0
-            heatmap(X, Y, Array(T)', aspect_ratio=1, xlims=(X[1], X[end]), ylims=(Y[1], Y[end]), c=:inferno, clims=(-0.1,0.1), title="T° (it = $it of $nt)")
-            Vxp = 0.5*(Vx[1:st:end-1,1:st:end  ]+Vx[2:st:end,1:st:end])
-            Vyp = 0.5*(Vy[1:st:end  ,1:st:end-1]+Vy[1:st:end,2:st:end])
-            Vscale = 1/maximum(sqrt.(Vxp.^2 + Vyp.^2)) * dx*(st-1)
-            quiver!(Xp[:], Yp[:], quiver=(Vxp[:]*Vscale, Vyp[:]*Vscale), lw=0.1, c=:blue); frame(anim)
+        T_inn .= Array(inn(T));   gather!(T_inn, T_v)
+        if mod(it,nout)==0 && me==0
+            heatmap(Xi_g, Zi_g, T_v[:,y_sl,:]', aspect_ratio=1, xlims=(Xi_g[1], Xi_g[end]), zlims=(Zi_g[1], Zi_g[end]), c=:inferno, clims=(-0.1,0.1), title="T° (it = $it of $nt)")
+            #Vxp = 0.5*(Vx[1:st:end-1,1:st:end  ,1:st:end  ]+Vx[2:st:end,1:st:end,1:st:end])
+            #Vyp = 0.5*(Vy[1:st:end  ,1:st:end-1,1:st:end  ]+Vy[1:st:end,2:st:end,1:st:end])
+            #Vzp = 0.5*(Vz[1:st:end  ,1:st:end  ,1:st:end-1]+Vz[1:st:end,1:st:end,2:st:end])
+            #Vscale = 1/maximum_g(sqrt.(Vxp.^2 + Vyp.^2 + Vzp.^2)) * dx*(st-1)
+            #quiver!(Xp[:], Zp[:], quiver=(Vxp[:]*Vscale, Vzp[:]*Vscale), lw=0.1, c=:blue);
+            frame(anim)
+            save_array("out_T", convert.(Float32, Array(T)))
             # display( quiver!(Xp[:], Yp[:], quiver=(Vxp[:]*Vscale, Vyp[:]*Vscale), lw=0.1, c=:blue) )
         end
     end
-    gif(anim, "ThermalConvect2D.gif", fps = 15)
+    gif(anim, "ThermalConvect3D.gif", fps = 15)
+    finalize_global_grid()
     return
 end
 
-ThermalConvection2D()
+ThermalConvection3D()
